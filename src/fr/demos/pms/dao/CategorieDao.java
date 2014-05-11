613683defa76f46e9ab68bb026eb024a3e64494f
ydn47 <yanndara@gmail.com>
2014-05-11 18:04:51 +0200

    Ajout de la méthode create pour les catégories

--- src/fr/demos/pms/dao/CategorieDao.java
+++ src/fr/demos/pms/dao/CategorieDao.java
@@ -3,6 +3,7 @@ package fr.demos.pms.dao;
 import java.util.Collection;
 
 import fr.demos.pms.model.Categorie;
+import fr.demos.pms.model.Client;
 
 /**
  * @author Yann-Dara Nong
@@ -11,4 +12,5 @@ public interface CategorieDao {
 	Collection<Categorie> findAllCategories();
 	String findNomById(long idCategorie);
 	long findIdByNom(String nomCategorie);
+	void create(Categorie c) throws DAOException;
 }
